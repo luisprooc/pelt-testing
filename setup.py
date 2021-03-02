55ac0c011f506cb9a4ad8a523650471feb6b4bac
Erick Sosa <ericksosagarcias@gmail.com>
2021-03-02 18:55:30 -0400

    chore: remove spanish description

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pelt-testing", # Replace with your own username
+    name="pelt-testing",
     version="0.0.1",
     author="Luis Rosario",
     author_email="luisprooc@gmail.com",
-    description="Este paquete nos permite testear algoritmos y generar datos de prueba para lo anteriormente dicho",
+    description="This package allows us to test algorithms and generate test data",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/luisprooc/pelt-testing",
